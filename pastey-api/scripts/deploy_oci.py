f5697e72f0a24f9735a688adcec552c793e9eb1f
Burak <burakduruk52@gmail.com>
2024-05-29 22:28:28 -0400

    python script for auto deployment to oci

--- pastey-api/scripts/deploy_oci.py
+++ pastey-api/scripts/deploy_oci.py
@@ -2,12 +2,14 @@ import oci
 import sys
 import os
 import time
+import requests
 
 #############################
 # ARGV[1] = compartment_id  #
 # ARGV[2] = subnet_id       #
 # ARGV[3] = image link      #
 # ARGV[4] = a domain        #
+# ARGV[5] = github sha      #
 #############################
 
 DISPLAY_NAME = "pastey-api"
@@ -38,7 +40,7 @@ deleted = []
 for container_instance in list_container_instances_response.data.items:
     if container_instance.lifecycle_state != "DELETED" and container_instance.display_name == DISPLAY_NAME:
         res = container_instances_client.delete_container_instance(container_instance.id)
-        print("Requested deletion of container instance: " + container_instance.id)
+        print("Requested deletion of container instance")
         deleted.append(container_instance.id)
 
 time.sleep(5)
@@ -48,7 +50,7 @@ while True:
     for container_instance_id in deleted:
         container_instance = container_instances_client.get_container_instance(container_instance_id).data
         if container_instance.lifecycle_state == "DELETED":
-            print("Container instance deleted: " + container_instance_id)
+            print("Container instance deleted")
             deleted.remove(container_instance_id)
 
     if len(deleted) == 0:
@@ -57,7 +59,6 @@ while True:
     time.sleep(10)
 
 print("All container instances deleted")
-print(sys.argv[3])
 
 # Create a new container instance
 create_container_instance_details = oci.container_instances.models.CreateContainerInstanceDetails(
@@ -66,7 +67,7 @@ create_container_instance_details = oci.container_instances.models.CreateContain
     display_name=DISPLAY_NAME,
     containers=[
         oci.container_instances.models.CreateContainerDetails(
-            image_url=sys.argv[3],
+            image_url=sys.argv[3]+":"+sys.argv[5],
         )
     ],
     shape=SHAPE,
@@ -78,7 +79,44 @@ create_container_instance_details = oci.container_instances.models.CreateContain
     ]
 )
 
-print(create_container_instance_details)
+create_container_instance_response = container_instances_client.create_container_instance(create_container_instance_details)
 
-#create_container_instance_response = container_instances_client.create_container_instance(create_container_instance_details)
-#print("Requested creation of container instance: " + create_container_instance_response.data.id)
+vnic_id = ""
+
+while True:
+    container_instance = container_instances_client.get_container_instance(create_container_instance_response.data.id).data
+    if container_instance.lifecycle_state == "ACTIVE":
+        print("Container instance running")
+        vnic_id = container_instance.vnics[0].vnic_id
+        break
+
+    time.sleep(10)
+
+
+core_client = oci.core.VirtualNetworkClient(config)
+get_vnic_response = core_client.get_vnic(vnic_id=vnic_id)
+
+public_ip = get_vnic_response.data.public_ip
+
+# Modify A record in DNS zone (Cloudflare API)
+headers = {
+    "Authorization": "Bearer " + os.getenv("CF_API_KEY"),
+    "Content-Type": "application/json"
+}
+
+data = {
+    "type": "A",
+    "name": "api.@",
+    "content": public_ip,
+    "ttl": 1,
+    "proxied": True
+}
+
+response = requests.put("https://api.cloudflare.com/client/v4/zones/" + os.getenv("CF_ZONE_ID") + "/dns_records/" + os.getenv("CF_DNS_RECORD_ID"), headers=headers, json=data)
+
+if response.json()["success"] != True:
+    print("Error updating DNS record")
+else:
+    print("DNS record updated")
+
+print("Deployment done")
